2d81a3f7616000aae072212b11f7bee023fbe273
Viktor Lee <viktor.lee@jublia.com>
2021-12-06 23:19:44 +0700

    Initial Commit

---
+++ Test2CI/Test2CIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test2CIApp.swift
+//  Test2CI
+//
+//  Created by Victor . on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Test2CIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
